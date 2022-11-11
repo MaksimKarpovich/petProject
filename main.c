f490061687e8d01e299ce3d14af6668df08e266d
Maksim Karpovich <Maksim.Karpovich@promwad.com>
2022-11-11 15:28:06 +0300

    feat: добавить функции перемещения каретки

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
@@ -11,4 +12,10 @@ int main(int argc, char** argv)
     printf ("\033[32m");
     drawPyramid(window.ws_row - 1);
     printf ("\033[0m");
+    printf ("\033[A");
+    printf ("\033[A");
+    printf ("\033[C");
+    printf ("\033[C");
+    printf ("123");
+//    system("clear");
 }
